3fe1ba75ec1363d6c65b93065a00186f894e038c
conjamver <connor.j.vernon97@gmail.com>
2020-08-11 22:15:31 +1000

    Updates

---
+++ precache-manifest.1fe1de64b993dc2f26275e224c9c3ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e80fc1f98bcb543f448d06cd3b2b99d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e00548d55e887135dea",
+    "url": "/portfolio/static/css/main.7421e622.chunk.css"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e00548d55e887135dea",
+    "url": "/portfolio/static/js/main.63ba3c3f.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
